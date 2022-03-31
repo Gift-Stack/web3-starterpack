c024d0dad7f7e1f8cb95372dd40d168b661017d4
Gifted <gkenny896@gmail.com>
2022-03-31 20:41:16 +0100

    Starter kit

---
+++ packages/app/src/components/home/index.tsx
@@ -0,0 +1,14 @@
+import Header from "components/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <div className="h-screen flex items-center justify-center">
+        <h1 className="text-3xl font-bold">NextJs Web3 Starter Kit.</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
